4d1983d145943e202edccb7a265d8ae7f8ec950c
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2020-12-07 12:47:44 -0500

    Haciendo cambios para la integración de Flask

--- app.py
+++ app.py
@@ -4,3 +4,24 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/recuperarcontrasena")
+def recuperarcontrasena():
+	return render_template("recuperarcontrasena.html")
+
+@app.route("/registro")
+def registro():
+	return render_template("registrarse.html")
+
+@app.route("/home")
+def home():
+	return render_template("home.html")
+
+@app.route("/subir")
+def subir():
+	return render_template("subir.html")
+
+@app.route("/perfil")
+def perfil():
+	return render_template("profile.html")
+
