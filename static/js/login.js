f6826b26c293199af9025645d00c503231377f6c
juansuarezr <68455654+juansuarezr@users.noreply.github.com>
2020-12-08 22:12:52 -0500

    Funcionalidad de login y recuperar contraseña

---
+++ static/js/login.js
@@ -0,0 +1,15 @@
+
+
+function mostrarContrasena(){
+    var tipo = document.getElementById("password");
+    tipo.type = "text"
+}
+
+function ocultarContrasena(){
+    var tipo = document.getElementById("password");
+    tipo.type = "password";
+
+    
+}
+
+
