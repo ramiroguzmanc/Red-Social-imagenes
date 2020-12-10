dc6897c19bb7c8cddd34c2a7b9e7abb326bd2842
juansuarezr <68455654+juansuarezr@users.noreply.github.com>
2020-12-09 22:13:01 -0500

    Integración con wtforms

--- static/js/login.js
+++ static/js/login.js
@@ -1,15 +1,13 @@
 
 
 function mostrarContrasena(){
-    var tipo = document.getElementById("password");
+    var tipo = document.getElementById('form.contraseña.id');
     tipo.type = "text"
 }
 
 function ocultarContrasena(){
-    var tipo = document.getElementById("password");
+    var tipo = document.getElementById('form.contraseña.id');
     tipo.type = "password";
-
-    
 }
 
 
