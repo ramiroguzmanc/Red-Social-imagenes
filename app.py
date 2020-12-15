937502dc9f14cad9a077442879b5b4473f6c42a5
Heri <hlondono@misena.edu.com>
2020-12-14 22:20:14 -0500

    Base de datosusuario y sesion

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 from forms import FormIndex, FormRecuperar, FormSubir, FormRegistro
 import yagmail
+import sqlite3
+import sys
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -13,9 +15,23 @@ app.secret_key = os.urandom(24)
 def index():
 
     form = FormIndex()
-    if(form.validate_on_submit()):
-        if form.usuario.data=='prueba' and form.contraseña.data=='prueba1234':
-            return redirect('/home')
+    if request.method == "POST":
+        usuario = form.usuario.data
+        contraseña = form.contraseña.data
+        with sqlite3.connect("redsocial.db") as con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM usuarios WHERE usuario=? AND contraseña=?", [usuario, contraseña])
+            registro = cur.fetchone()
+            if registro:
+                if registro[8]:
+                    return render_template('home.html')
+                else:
+                    flash('Usuario no activo')
+                    return redirect('/')
+            else:
+                flash('Usuario o contraseña incorrectos')
+                return redirect('/')
+
     return render_template('index.html', form=form)
 
 
@@ -27,12 +43,27 @@ def login():
 @app.route('/registro',methods=['POST','GET'])
 def registro():
     form = FormRegistro()
-    if(form.validate_on_submit()):
-        yag = yagmail.SMTP('imacol.misiontic@gmail.com','misiontic')
-        yag.send(to=form.email.data, subject='Activa tu cuenta', contents = 'Activa tu cuenta ('+ request.method +')')
-        #En esta parte se guardará el usuario en la base de datos
-        return redirect('/')
+    if request.method == "POST":
+        nombre = form.nombre.data
+        apellido = form.apellido.data
+        email = form.email.data
+        fecha = request.form['fecha']
+        usuario = form.usuario.data
+        contraseña = form.contraseña.data
+        sexo = request.form['sexo']
+        try:
+            with sqlite3.connect("redsocial.db") as con:
+                cur = con.cursor() #manipula la conexión a la bd
+                cur.execute("INSERT INTO usuarios VALUES (null,?,?,?,?,?,?,?,False)", (nombre,apellido,email,fecha,usuario,contraseña,sexo))
+                con.commit()# confirma la transacción
+            yag = yagmail.SMTP('imacol.misiontic@gmail.com','misiontic')
+            yag.send(to=email, subject='Activa tu cuenta', contents = "Activa tu cuenta dando click <a href='http://localhost:5000/activar?usuario=" + usuario + "'> aqui</a>")
+            flash('Por favor revise el correo para activar su cuenta')
 
+            return redirect('/')
+        except:
+            con.rollback()
+            return 'No se pudo guardar' + sys.exc_info()[1].args[0]
 
     return render_template('registrarse.html', form=form)
 
@@ -67,6 +98,21 @@ def subir():
 @app.route('/perfil',methods=['GET','POST'])
 def perfil():
     return render_template('profile.html')
+
+@app.route('/activar')
+def activar():
+    usuario = request.args.get('usuario')
+    try:
+        with sqlite3.connect("redsocial.db") as con:
+            cur = con.cursor() #manipula la conexión a la bd
+            cur.execute("UPDATE usuarios SET activo=True WHERE usuario=?", [usuario])
+            con.commit()# confirma la transacción
+        flash('Usuario activado, ya puede ingresar sesion')
+        return redirect('/')
+    except:
+        con.rollback()
+        return 'No se pudo guardar' + sys.exc_info()[1].args[0]
+
 
 
 if __name__=='__main__':
