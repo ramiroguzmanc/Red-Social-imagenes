3d4593c4d4922912415fc8d05d90e174af3ab686
Heri <hlondono@misena.edu.com>
2020-12-15 23:47:25 -0500

    Sesion y carga de imagen

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from wtforms import StringField
 from flask_wtf import FlaskForm
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import os
 from forms import FormIndex, FormRecuperar, FormSubir, FormRegistro
 import yagmail
 import sqlite3
 import sys
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 
 @app.route('/',methods=['GET', 'POST'])
@@ -26,6 +28,9 @@ def index():
             if registro:
                 if registro[8]:
                     if check_password_hash(registro[6], contraseña):
+                        session.clear()
+                        session["usuario"] = registro[5]
+                        session["id"] = registro[0]
                         return render_template('home.html')
                     else:
                         flash('Usuario o contraseña incorrectos')
@@ -59,7 +64,7 @@ def registro():
         try:
             with sqlite3.connect("redsocial.db") as con:
                 cur = con.cursor() #manipula la conexión a la bd
-                cur.execute("INSERT INTO usuarios VALUES (null,?,?,?,?,?,?,?,False)", (nombre,apellido,email,fecha,usuario,contraseña,sexo))
+                cur.execute("INSERT INTO usuarios VALUES (null,?,?,?,?,?,?,?,False,'usuario')", (nombre,apellido,email,fecha,usuario,contraseña,sexo))
                 con.commit()# confirma la transacción
             yag = yagmail.SMTP('imacol.misiontic@gmail.com','misiontic')
             yag.send(to=email, subject='Activa tu cuenta', contents = "Activa tu cuenta dando click <a href='http://localhost:5000/activar?usuario=" + usuario + "'> aqui</a>")
@@ -92,13 +97,33 @@ def home():
 @app.route('/subir',methods=['POST','GET'])
 def subir():
     form = FormSubir()
+    if request.method == "POST":
+        if 'imagen' not in request.files:
+            flash('No hay parte de archivo')
+            return redirect('/subir')
+        f = request.files['imagen']
+        if f.filename == '':
+            flash('Archivo no seleccionado')
+            return redirect('/subir')
 
-    if (form.validate_on_submit()):
-        #En esta parte se guardará la imagen
-        return redirect('/home')
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+        propietario_id = session["id"]
+        nombre = form.nombre.data
+        tags = form.tags.data
+        archivo = str(filename)
+        restriccion = form.esPublica.data
+        try:
+            with sqlite3.connect("redsocial.db") as con:
+                cur = con.cursor() #manipula la conexión a la bd
+                cur.execute("INSERT INTO imagenes VALUES (null,?,?,?,?,?)", (propietario_id,nombre,tags,archivo,restriccion))
+                con.commit()# confirma la transacción
+            return redirect('/home')
+        except:
+            con.rollback()
+            return 'No se pudo guardar' + sys.exc_info()[1].args[0]
     return render_template('subir.html', form=form)
-
 
 @app.route('/perfil',methods=['GET','POST'])
 def perfil():
